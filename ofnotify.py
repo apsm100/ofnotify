b138e17e49f505336fd1c7631b530ef170d15e8a
apsm100 <amritmanhas11@gmail.com>
2021-04-23 09:06:05 -0700

    Clean up var

--- ofnotify.py
+++ ofnotify.py
@@ -5,10 +5,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import json
 import re
-version = "1.3-7"
+version = "1.3-8"
 print("ofnotify v" + version + "\nAmrit Manhas")
 
-# Email configuration (set to outlook\hotmail).
+# Email configuration (set to Outlook by default).
 # Input your own email and password, this script will send it from yourself.
 email = ""
 password = ""
@@ -17,21 +17,25 @@ def main():
     """Main drives the program.
     
     Uses Beautifulsoup to scrape zero post listings from the 
-    OF sales forum, extract their id's, compares the id's with a stored id list (last run), 
-    notifies the user of new posts via email, and outputs the current id list to a json. 
+    OF sales forum, extract their IDs, compares the IDs with a stored ID list (last run), 
+    notifies the user of new posts via email, and outputs the current ID list to a json. 
     """
     soup = getSoup()
-    title_list, link_list, index_list = getLists(soup)
-    final_title, final_link, new_id_list = compareLists(title_list, link_list, index_list)
-
-    if final_title:
+    # link_list - list of listing links.
+    # title_list - list of listing titles.
+    # index_list - list of indexes where the listing post count is zero.
+    link_list, title_list, index_list = getLists(soup)
+    #final_link_list - list of listing links after comparison.
+    #final_title_list - list of listing titles after comparison.
+    #new_id_list - list of listing IDs derived from the final_link_list. 
+    final_link_list, final_title_list, new_id_list = compareLists(link_list, title_list, index_list)
+    if final_link_list:
         print('\nNew items found:')
-        print(final_link)
-        print(final_title)
-        sendNotification(final_title, final_link, new_id_list)
+        print(final_title_list)
+        print(final_link_list)
+        sendNotification(final_link_list, final_title_list, new_id_list)
     else:
         print('\nNothing to tell you.')
-    
     dumpList(new_id_list)
 
 def getSoup():
@@ -45,17 +49,16 @@ def getSoup():
     return soup
 
 def getLists(soup):
-    """Scrapes the html for titles and links and returns 
-    a list of titles, a list of links, and a list of indexes where a listing has zero posts."""
-    title_list = []
+    """Scrapes the html for links and titles and returns 
+    a list of links, a list of titles, and a list of indexes where a listing has zero posts."""
     link_list = []
-    links = soup.find_all(href=True)  # Get all href elements.
-    for i in links:
+    title_list = []
+    titles = soup.find_all(href=True)  # Get all href elements.
+    for i in titles:
         if 'threads/' in i['href']:
-            title_list.append(i['href'])
-            link_list.append(i.string)
-
-    # post_list finds just the post count, index_list will hold the zero post indexes.
+            link_list.append(i['href'])
+            title_list.append(i.string)
+    # post_list finds just listing post counts, index_list will hold the zero post indexes.
     post_list = soup.find_all('dl', class_="major")
     index_list = []  # List of indexes for zero post listings.
     index_count = -1  # Used for items in index_list.
@@ -65,12 +68,12 @@ def getLists(soup):
             index_count = index_count + 1
             if v.contents == ['0']: # If the listing has zero posts.
                 index_list.append(index_count)
-    return title_list, link_list, index_list
+    return link_list, title_list, index_list
 
-def compareLists(title_list, link_list, index_list):
-    """Creates a new title and links list of zero post listings 
+def compareLists(link_list, title_list, index_list):
+    """Creates a new link and titles list of zero post listings 
     using index_list. A new id list is created and is compared with the old id list.
-    ID's are used here as titles and links can change, but listing id's will never change.
+    ID's are used here as links and titles can change, but listing id's will never change.
     Return new items in new lists."""
     # Check for saved id.json and load for comparison. 
     try:
@@ -78,55 +81,47 @@ def compareLists(title_list, link_list, index_list):
             old_id_list = json.load(f)
     except:
         old_id_list = []
-
-    new_title_list = []
     new_link_list = []
-
+    new_title_list = []
     # Use index_list to create zero-post only lists.
     for i in index_list:
-        new_title_list.append(title_list[i])
         new_link_list.append(link_list[i])
-
-    # Create a new_id_list by using new_title_list urls.
+        new_title_list.append(title_list[i])
+    # Create a new_id_list by using new_link_list urls.
     new_id_list = []
     new_index_list = []
-    for i in new_title_list:
+    for i in new_link_list:
         result = re.search('\.(.*?)/', i)
         new_id_list.append(result.group(1))
-
     # Compare new_id_list with old_id_list and append new listing indexes to new_index_list.
     for i in new_id_list:
         if i not in old_id_list:
             new_index_list.append(new_id_list.index(i))
-
-    final_title = []
-    final_link = []
-
-    # Use new_index_list indexes to create final_title and final_link.
+    final_link_list = []
+    final_title_list = []
+    # Use new_index_list indexes to create final_link_list and final_title_list.
     for i in new_index_list:
-        final_title.append(new_title_list[int(i)])
-        final_link.append(new_link_list[int(i)])
-    return final_title, final_link, new_id_list
+        final_link_list.append(new_link_list[int(i)])
+        final_title_list.append(new_title_list[int(i)])
+    return final_link_list, final_title_list, new_id_list
 
-def sendNotification(final_title, final_link, new_id_list):
+def sendNotification(final_link_list, final_title_list, new_id_list):
     """Creates and sends the html notification.
     The listing photos are added if possible."""
     html_list = []
-    difference = len(final_title)
+    difference = len(final_link_list)
     index_range = range(0, difference)
-    versionnum = version
-
+    ver = version
     for i in index_range:
         if i == difference:
             break
-
         # Variables for email.
-        nam = final_link[i]
-        url = "https://omegaforums.net/" + final_title[i]
+        title = final_title_list[i]
+        url = "https://omegaforums.net/" + final_link_list[i]
 
-        imageLink = getImage(url)
+        image_link = getImage(url)
         # If an image is not available, hide it and adjust the view. 
-        if imageLink:
+        if image_link:
             display = ""
             em = 0
         else:
@@ -142,17 +137,16 @@ def sendNotification(final_title, final_link, new_id_list):
             border = "display:none;"
         else:
             border = ""
-        
-        html_list_item = """<div style="margin-top:0.5em;margin-bottom:0.5em;"><a href="{url}" style="text-decoration:none;"><img src="{imageLink}" style="{display}object-fit:cover;width:100%;max-height:330px;border-radius:18px;margin-top:0.5em;margin-bottom:1em;"><h3 style="margin-top:{em}em; margin-bottom:{em}em;color:#000000;padding-left:6px;padding-right:6px;">{nam}</h3></a></div><div style="{border}margin-bottom:2em;margin-top:2em;height:1px;width:100%;background-color:gainsboro;"></div>""".format(**locals())
+        html_list_item = """<div style="margin-top:0.5em;margin-bottom:0.5em;"><a href="{url}" style="text-decoration:none;">
+        <img src="{image_link}" style="{display}object-fit:cover;width:100%;max-height:330px;border-radius:18px;margin-top:0.5em;margin-bottom:1em;">
+        <h3 style="margin-top:{em}em; margin-bottom:{em}em;color:#000000;padding-left:6px;padding-right:6px;">{title}</h3></a></div>
+        <div style="{border}margin-bottom:2em;margin-top:2em;height:1px;width:100%;background-color:gainsboro;"></div>""".format(**locals())
         html_list.append(html_list_item)
-        
         body = '\n'.join(html_list)
-
     if difference > 1:  # Plurality or not.
         period = "s"
     else:
         period = ""
-
     html = """\
     <html>
         <head></head>
@@ -164,32 +158,29 @@ def sendNotification(final_title, final_link, new_id_list):
                 &nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
                 </div>
                 {body}
-                <footer style="margin-top:3em;color:gainsboro;text-align:center;font-size:60%;">ofnotify v{versionnum}</footer>
+                <footer style="margin-top:3em;color:gainsboro;text-align:center;font-size:60%;">ofnotify v{ver}</footer>
             </body>
     </html>""".format(**locals())
     sendEmail(html)
 
 def getImage(url):
-    """Gets and returns the imageLink at the specified url.
+    """Gets and returns the imagetitle at the specified url.
     There are a few methods of uploading a photo to OF, the following three
     cover the methods observed."""
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-
     # If photo was attached via OF server.
     messageContent = soup.find("div", {"class": "messageContent"})
     image = messageContent.find('img', {"loading": "lazy"})
-    
-    # If photo was hyperlinked.
+    # If photo was hypertitleed.
     if not image:
         image = messageContent.find('img', {"class": "bbCodeImage LbImage"})
-    
-    # If an image was found create a link.
+    # If an image was found create a title.
     if image:
-        imageLink = "https://omegaforums.net/" + image['src']
+        imagetitle = "https://omegaforums.net/" + image['src']
     else:
-        imageLink = ""
-    return imageLink
+        imagetitle = ""
+    return imagetitle
 
 def sendEmail(html):
     """Sends an html email to the specified username and email
